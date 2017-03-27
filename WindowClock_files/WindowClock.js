6066eb0d65910cd7bec835fca329182ee66dc130
Magnetite <spaceman389@yahoo.com>
2017-03-26 22:28:58 -0500

    arrTime() returns single digit time array
    arrTime function in WindowClock.js returns an array of chars, each a
    digit of a conventional digital clock.
    Example: 10:33 would be ['1','0','3','3'].

--- WindowClock_files/WindowClock.js
+++ WindowClock_files/WindowClock.js
@@ -1,42 +1,36 @@
  
 
-var intWords = ["", "one","two","three","four","five","six",'seven','eight','nine','ten','eleven',
-'twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
-
-var tensWords = ['twenty','thirty','fourty','fifty'];
+var intPat = [];  //<= todo fill array with hexadecimal values that correspond to div ids 
 
 
 function arrTime(){
 
 var d = Date();
 
-var timeArr = d.split(" ")[4].split(":");
+var ta = d.split(" ")[4].split(":").join("").split(""); // makes an array of single digit strings
 
-return timeArr;
+var h = ta.shift();
+h += ta.shift();
+
+var hour = parseInt(h);
+
+if (hour > 12){
+	hour -= 12;
+} else if (hour === 0){
+	hour = 12;
+}
+
+return hour.toString().split("").concat(ta);
+
+
 
 }
 
-function translate(m, mode){
+function buildNumeral(m){
 
 var w = "";
 
-if (m >= 20 && m < 30){
-		w = tensWords[0] + " " + intWords[(m % 10) ];
-	} else if (m >= 30 && m < 40){
-		w = tensWords[1] + " " + intWords[(m % 10) ];
-	} else if (m >= 40 && m < 50){
-		w = tensWords[2] + " " + intWords[(m % 10) ];
-	} else if (m >= 50){
-		w = tensWords[3] + " " + intWords[(m % 10) ];
-	} else if (m < 20 && m > 9){
-		w = intWords[m];
-	} else if (m > 0 && m < 10){
-		w = "O' " + intWords[m];
-	} else if (mode === 1) {
-		w = "O'Clock";
-	} else {
-		w = "";
-	}
+//todo code below
 	
 	return w;
 }
@@ -47,27 +41,21 @@ function sayTime(){
 	
 	var hour = "", min = "", ampm = "", sec = "";
 	
-	var h = parseInt(t[0]);
-	var m = parseInt(t[1]);
-	var s = parseInt(t[2]);
+	
 	
     if (h > 12){
-        hour = intWords[h - 12];
+        hour = intPat[h - 12];
         ampm = "pm";
     } else if ( h === 0){
-		hour = intWords[12];
+		hour = intPat[12];
 		ampm = "am";
 	} else {
-        hour = intWords[h];
+        hour = intPat[h];
         ampm = "am";
     }
 	
-	min = translate(m, 1);
-	sec = translate(s, 0);
 	
 	
-	document.getElementById("time").innerHTML = "<h1>" + hour + " " + min + " " + ampm + "</h1>";
-	document.getElementById("sec").innerHTML = "<h1>" + sec + "</h1>";
 	setTimeout(sayTime, 1000);
 }
 
